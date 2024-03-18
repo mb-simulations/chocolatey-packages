d80eb704f56d3a3784b47a24e43560a233a2586c
Marco Baehler <marco@mbaehler.ch>
2024-03-18 23:13:07 +0100

    Initial commit

---
+++ manual/vPilot/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://vpilot.rosscarlson.dev/Assets/Files/Installers/vPilot-Setup-3.8.1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'vPilot*'
+
+  checksum      = '88B8B061A3E96580CC1083CFEE4B01B549376AF271618D267FC03C7919F22F61'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
